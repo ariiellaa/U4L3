6ad63455a3cec4e37643f366dffce7243b1561f6
ariiellaa <44405336+ariiellaa@users.noreply.github.com>
2019-02-04 11:29:33 -0500

    Add files via upload

---
+++ Lesson 3/Problem 2/problem2.py
@@ -0,0 +1,5 @@
+for x in range(1000,0,-1):
+	print(x)
+	
+print("Blast off")
+
---
+++ Lesson 3/Problem 3/problem3.py
@@ -0,0 +1,5 @@
+for x in range (-5,5):
+	print ('x =' + str(x))
+	y = -2 * x ** 2 + 4 * x -3 
+	print ('y =' + str(y))
+	print()
